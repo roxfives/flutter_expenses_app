e69232ea789220959be785abd509b0faf817ac83
Ricardo Chagas <ricardo.chagas2@outlook.com>
2020-04-09 17:58:38 -0300

    Builds the list of transactions using the ListView

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -3,66 +3,68 @@ import 'package:intl/intl.dart';
 
 import 'package:expenses_app/models/transaction.dart';
 
-
 class TransactionList extends StatelessWidget {
-  
   final List<Transaction> _transactions;
-
 
   const TransactionList(this._transactions);
 
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: _transactions.map((tx) {
-        return Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                child: Text(
-                  NumberFormat.simpleCurrency().format(tx.amount),
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple,
-                  ),
-                ),
-                padding: EdgeInsets.all(10),
-                margin: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 15,
-                ),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.purple,
-                    width: 2,
-                  ),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    tx.title,
+    return Container(
+      height: 134,
+      child: ListView.builder(
+        itemBuilder: (ctx, pos) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    NumberFormat.simpleCurrency()
+                        .format(_transactions.elementAt(pos).amount),
                     style: TextStyle(
-                      fontSize: 16,
                       fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
                     ),
                   ),
-                  Text(
-                    DateFormat.yMMMd().format(tx.date),
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 10,
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
                     ),
                   ),
-                ],
-              ),
-            ],
-          ),
-        );
-      }).toList(),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      _transactions.elementAt(pos).title,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      DateFormat.yMMMd()
+                          .format(_transactions.elementAt(pos).date),
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 10,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+        itemCount: _transactions.length,
+      ),
     );
   }
 }
