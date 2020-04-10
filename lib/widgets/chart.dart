da01d6449af277007de5d26f4255e26fb6d3b271
Ricardo Chagas <ricardo.chagas2@outlook.com>
2020-04-10 18:13:40 -0300

    Fixes the method call

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -24,7 +24,7 @@ class Chart extends StatelessWidget {
       }
 
       return {
-        'day': DateFormat.E(weekDay),
+        'day': DateFormat.E().format(weekDay),
         'amount': totalSum,
       };
     });
