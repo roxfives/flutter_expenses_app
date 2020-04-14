67632335c7951c7e66430b535e5161e88389ca7a
Ricardo Chagas <ricardo.chagas2@outlook.com>
2020-04-14 11:34:23 -0300

    Makes the list of transactions take all available space

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,73 +11,75 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      child: _transactions.isEmpty
-          ? Column(
-              children: <Widget>[
-                Text(
-                  'No transactions added yet!',
-                  style: Theme.of(context).textTheme.title,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  height: 200,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
+    return Expanded(
+      child: Container(
+        child: _transactions.isEmpty
+            ? Column(
+                children: <Widget>[
+                  Text(
+                    'No transactions added yet!',
+                    style: Theme.of(context).textTheme.title,
                   ),
-                ),
-              ],
-            )
-          : ListView.builder(
-              itemBuilder: (ctx, pos) {
-                return Card(
-                  margin: EdgeInsets.symmetric(
-                    horizontal: 5,
-                    vertical: 8,
+                  SizedBox(
+                    height: 10,
                   ),
-                  child: ListTile(
-                    title: Text(
-                      _transactions.elementAt(pos).title,
-                      style: Theme.of(context).textTheme.title,
+                  Container(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
                     ),
-                    subtitle: Text(
-                      DateFormat.yMMMd()
-                          .format(_transactions.elementAt(pos).date),
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 10,
+                  ),
+                ],
+              )
+            : ListView.builder(
+                itemBuilder: (ctx, pos) {
+                  return Card(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 5,
+                      vertical: 8,
+                    ),
+                    child: ListTile(
+                      title: Text(
+                        _transactions.elementAt(pos).title,
+                        style: Theme.of(context).textTheme.title,
                       ),
-                    ),
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6),
-                        child: FittedBox(
-                          child: Text(
-                            NumberFormat.simpleCurrency()
-                                .format(_transactions.elementAt(pos).amount),
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                              color: Theme.of(context).accentColor,
+                      subtitle: Text(
+                        DateFormat.yMMMd()
+                            .format(_transactions.elementAt(pos).date),
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10,
+                        ),
+                      ),
+                      leading: CircleAvatar(
+                        radius: 30,
+                        child: Padding(
+                          padding: const EdgeInsets.all(6),
+                          child: FittedBox(
+                            child: Text(
+                              NumberFormat.simpleCurrency()
+                                  .format(_transactions.elementAt(pos).amount),
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Theme.of(context).accentColor,
+                              ),
                             ),
                           ),
                         ),
                       ),
+                      trailing: IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Theme.of(context).errorColor,
+                          onPressed: () => _deleteTransaction(
+                              _transactions.elementAt(pos).id)),
                     ),
-                    trailing: IconButton(
-                        icon: Icon(Icons.delete),
-                        color: Theme.of(context).errorColor,
-                        onPressed: () => _deleteTransaction(_transactions.elementAt(pos).id)),
-                  ),
-                );
-              },
-              itemCount: _transactions.length,
-            ),
+                  );
+                },
+                itemCount: _transactions.length,
+              ),
+      ),
     );
   }
 }
